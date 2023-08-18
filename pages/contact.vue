ff2112ed6817e454fe352dc5ce32881b371c9077
Jacob Ehlert <jacobdukc@gmail.com>
2023-08-18 14:19:58 -0700

    Added contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,24 +1,31 @@
 <template>
-	<div class="flex gap-[32px] h-full">
-		<PagesNav selected="work" />
+	<div class="flex gap-[32px] min-h-0">
+		<PagesNav selected="contact" />
 		<div
-			class="w-full overflow-y-auto h-full grid grid-cols-3 max-[1270px]:grid-cols-2 max-[1000px]:grid-cols-1 gap-[24px] p-[32px] bg-[#0f0f0f]/60 border border-white/10 backdrop-blur-[100px] rounded-[10px]"
+			class="w-full overflow-hidden h-full flex bg-[#0f0f0f]/60 border border-white/10 backdrop-blur-[100px] rounded-[10px]"
 		>
-			<Post />
-			<Post />
-			<Post />
-			<Post />
-			<Post />
-			<Post />
-			<Post />
+			<div class="flex flex-col place-content-between p-[64px] h-full w-full">
+				<div class="flex flex-col gap-[32px] h-fit w-full">
+					<h1 class="text-[40px] font-medium">Contact</h1>
+					<p class="text-[18px] leading-[225%]">
+						I am not available to work right now.
+						But if you need to contact me, do it via <b>Discord</b> or <b>Email</b>.
+					</p>
+				</div>
+				<div class="flex flex-col gap-[16px]">
+					<p class="text-[18px]">Discord: dukkcc (dukc#1881)</p>
+					<p class="text-[18px]">Email: jacobdukc@gmail.com</p>
+				</div>
+			</div>
+			<nuxt-img src="/contact.png" class="w-[50%] object-cover h-screen" />
 		</div>
 	</div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 useSeoMeta({
-	title: "DuckyHQ : Work",
-	ogTitle: "DuckyHQ : Work",
+	title: "DuckyHQ : Contact",
+	ogTitle: "DuckyHQ : Contact",
 	description:
 		"I'm dukc. And I'm a versatile designer skilled in UI, graphic, & motion design",
 	ogDescription:
