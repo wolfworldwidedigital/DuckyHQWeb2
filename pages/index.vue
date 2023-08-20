0a3b20c1255f768d0a6eb502af35175b9f4c2612
Jacob Ehlert <jacobdukc@gmail.com>
2023-08-20 16:38:10 -0700

    Made image a variale, did some final changes

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
 						v-for="post in list"
 						:key="post._path"
 						:to="post._path"
-						:style="{ 'background-image': post.img }"
+						:style="{ 'backgroundImage': `linear-gradient(to bottom,rgba(0,0,0,0),rgba(0,0,0,0.3)),url(${post.img})` }"
 						class="bg-cover hover:scale-[.98] cursor-pointer active:scale-75 transition-all duration-200 bg-no-repeat bg-clip-padding rounded-[10px] border border-white/10 w-full h-[400px] flex flex-col gap-[8px] p-[32px] align-bottom place-content-end group"
 					>
 						<h2
