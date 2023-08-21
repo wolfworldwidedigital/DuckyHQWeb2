9542d6bbf02d77fa6d6386c41336640b6313c1d9
Jacob Ehlert <jacobdukc@gmail.com>
2023-08-21 14:42:57 -0700

    Transferred all posts over to new website.

--- pages/posts/[slug].vue
+++ pages/posts/[slug].vue
@@ -5,7 +5,7 @@
 			class="w-full overflow-y-auto flex align-middle place-content-center overflow-x-hidden p-[32px] bg-[#0f0f0f]/60 border border-white/10 backdrop-blur-[100px] rounded-[10px]"
 		>
 			<article
-				class="prose lg:prose-xl prose-img:rounded-[10px] prose-h1:text-white prose-h2:text-white prose-h3:text-white prose-h4:text-white prose-h6:text-white prose-p:text-[#e1e1e1] prose-a:text-[#4abeff] hover:prose-a:text-[#a4deff] prose-code:text-[#b3b3b3] prose-li:text-[#e1e1e1] prose-ul:text-[#e1e1e1] prose-blockquote:text-[#e1e1e1]"
+				class="prose lg:prose-xl prose-img:rounded-[10px] prose-h1:text-white prose-h2:text-white prose-h3:text-white prose-h4:text-white prose-h6:text-white prose-p:text-[#e1e1e1] prose-a:text-white prose-a:no-underline hover:prose-a:text-white prose-code:text-[#b3b3b3] prose-li:text-[#e1e1e1] prose-ul:text-[#e1e1e1] prose-blockquote:text-[#e1e1e1]"
 			>
 				<ContentDoc />
 			</article>
