8d18d76dfa6c0fc7c36c7def93bca857cfe7cc48
Jacob Ehlert <jacobdukc@gmail.com>
2023-08-22 09:19:40 -0700

    fixed mobile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: false },
+	devtools: {enabled: false},
 	modules: [
 		"@nuxtjs/tailwindcss",
 		"@nuxt/image",
 		"nuxt-icons",
 		"@nuxt/content",
-		"floating-vue/nuxt"
+		"floating-vue/nuxt",
 	],
+	app: {
+		head: {
+			charset: "utf-8",
+			viewport: "width=device-width, initial-scale=1",
+		},
+	},
 });
